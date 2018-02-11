3ffe33a1ea66c67cb5e0f113b0b4bfc3b9131e4d
jaforewit <marcda495@gmail.com>
2018-02-10 21:49:31 -0500

    updated main to be a better example

--- Network/src/Main.java
+++ Network/src/Main.java
@@ -6,21 +6,24 @@ import TrainSet.*;
 public class Main {
     public static void main(String[] args) {
 
-        Network net = new Network(3, 3, 2);
+        Network net = new Network(10, 10, 10);
 
         TrainSet trainSet = new TrainSet(net.INPUT_SIZE,net.OUTPUT_SIZE);
-        double[] input1 = {10, -5, 2}, target1 = {1, 0};
-        double[] input2 = {3, 6, -4}, target2 = {0, 1};
+        double[] input1 = {10,-5,2,5,5,4,4,8,8,-2};
+        double[] target1 = {1,0,1,0,1,0,1,0,1,0};
+        double[] input2 = {6,-4,5,2,2,8,8,4,4,2};
+        double[] target2 = {0,1,0,1,0,1,0,1,0,1};
         trainSet.addData(input1, target1);
         trainSet.addData(input2, target2);
 
+        long time = System.nanoTime();
         net.train(trainSet,10000,2,0.3);
+        time = System.nanoTime() - time;
 
-        System.out.println("Result");
-        System.out.println(Arrays.toString(net.calculateOutput(input1)));
+        System.out.println("Result: " + Arrays.toString(net.calculateOutput(input1)));
+        System.out.println("MSE: " + net.MSE(input1, target1));
+        System.out.println("Result: " + Arrays.toString(net.calculateOutput(input2)));
         System.out.println("MSE: " + net.MSE(input2, target2));
-        System.out.println("Result");
-        System.out.println(Arrays.toString(net.calculateOutput(input1)));
-        System.out.println("MSE: " + net.MSE(input2, target2));
+        System.out.println("training time (ms): " + time/1000000);
     }
 }
