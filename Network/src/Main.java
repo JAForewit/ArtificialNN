a3d4008c193a0436de89f4dcb526608110d65b7b
jaforewit <marcda495@gmail.com>
2018-02-10 21:25:53 -0500

    updated main to be a better example

--- Network/src/Main.java
+++ Network/src/Main.java
@@ -7,22 +7,20 @@ public class Main {
     public static void main(String[] args) {
 
         Network net = new Network(3, 3, 2);
-        TrainSet trainSet = new TrainSet(3,2);
 
-        double[] input = new double[]{10, -5, 2};
-        double[] target = new double[]{1, 0};
-        double[] input2 = new double[]{2, -1, 4};
-        double[] target2 = new double[]{0, 1};
+        TrainSet trainSet = new TrainSet(net.INPUT_SIZE,net.OUTPUT_SIZE);
+        double[] input1 = {10, -5, 2}, target1 = {1, 0};
+        double[] input2 = {3, 6, -4}, target2 = {0, 1};
+        trainSet.addData(input1, target1);
+        trainSet.addData(input2, target2);
 
-        trainSet.addData(input,target);
-        trainSet.addData(input2,target2);
+        net.train(trainSet,10000,2,0.3);
 
-        net.train(trainSet,100000,2,0.3);
-
-        System.out.println("Results: ");
-        System.out.println(Arrays.toString(net.calculateOutput(input)));
-        System.out.println("Error: " + net.MSE(input, target));
-        System.out.println(Arrays.toString(net.calculateOutput(input2)));
-        System.out.println("Error: " + net.MSE(input2, target2));
+        System.out.println("Result");
+        System.out.println(Arrays.toString(net.calculateOutput(input1)));
+        System.out.println("MSE: " + net.MSE(input2, target2));
+        System.out.println("Result");
+        System.out.println(Arrays.toString(net.calculateOutput(input1)));
+        System.out.println("MSE: " + net.MSE(input2, target2));
     }
 }
